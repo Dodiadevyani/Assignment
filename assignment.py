1be42e7d381f00c94942f535a4e6e4faf4b4c5b4
Dodiadevyani <devd0093@gmail.com>
2016-11-12 14:53:33 +0530

    Update assignment.py
    first I read kisanhub.csv file. I found there is missing values, to find that values I used linear interpolation method. After that I resample whole   hourly data sets to daily data of max and min temperature.  And calculate daily mean of whole data sets and save this data in test.csv file.

--- assignment.py
+++ assignment.py
@@ -6,5 +6,17 @@ df=df.reindex(pd.date_range("01/01/2014", "31/05/2016",freq='H'), fill_value="Na
 df=df.astype(float)
 df=df.interpolate(method='linear', axis=0).ffill().bfill()
 print df
+maximum=df.resample('24H').max()
+minimum=df.resample('24H').min()
+#print maximum
+#print minimum
+mean=(maximum+minimum)/2
+#print mean
+target = open("test.csv", 'w')
+target.truncate()
+mean.to_csv('test.csv', mode='a', header=False)
+user_cols=['date', 'value']
+dm=pd.read_csv ("test.csv", header=None, names=user_cols)
+dm.head()
 
 
