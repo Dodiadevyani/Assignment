59c111d97be22eb777761c5910f63f36ea2de266
Dodiadevyani <devd0093@gmail.com>
2016-11-22 14:30:38 +0530

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -48,6 +48,43 @@ def minimum_temp(temp):
 def mean_temp(maximum_temp, minimum_temp):
     return (maximum_temp+minimum_temp)/2
 
+def meantemp_from_tempdatafrme():
+    # first read kisanhub.csv weather file using reading_weather_dataframe function and print weather dataframe
+    print "Weather data of kisanhub.csv file is: " 
+    weather_dataframe= reading_weather_dataframe ()
+    print weather_dataframe
+    print weather_dataframe.dtypes
+    print type(weather_dataframe)
+    print "After calculating missing weather data using linear interpolation method, Corrected weather data is:  "
+    # calculate missing weather data using finding_missing_data function and print corrected weather dataframe
+    corrected_weather_dataframe= finding_missing_data (weather_dataframe, "01/01/2014", "31/05/2016")
+    print corrected_weather_dataframe 
+    print corrected_weather_dataframe.dtypes
+    print type( corrected_weather_dataframe)
+    # to get daily dataframe from hourly weather dataframe resampling done using resample_dataframe function
+    resample_data= resample_dataframe( corrected_weather_dataframe)
+    print resample_data
+    print type( resample_data)
+    print "Daily maximum temperature of given time series:  "
+    # to get daily maximum temperature of given datetime series maximum_temp function used
+    max_temp=  maximum_temp(resample_data)
+    print max_temp
+    print max_temp.dtypes
+    print type( max_temp)
+    print "Daily minimum temperature of given time series:  "
+    # daily minimum temperature get using minimum_temp function
+    min_temp= minimum_temp(resample_data)
+    print min_temp
+    print min_temp.dtypes
+    print type(min_temp)
+    print "Daily mean temperature of given time series:  "
+    # to get daily mean temperature of given datetime series mean_temp function used
+    meantemp=mean_temp(max_temp, min_temp)
+    return meantemp
+    print meantemp.dtypes
+    print type(meantemp)
+
+       
 # For Users to give Planting and Harvesting date of crop
 def user_planting_date():
     print "Planting Date of crop is : "
@@ -83,7 +120,7 @@ def user_base_temp():
         print "Error: Please give numbers only"
         base_Temp= user_base_temp()
 
-# to calculate growth duration of crop using growth_durationof_crop function
+# to calculate growth duration of crop planting and harvesting date used
 def growth_durationof_crop(planting_date, harvesting_date,freq='D'):
     return pd.date_range(planting_date, harvesting_date,freq='D')
 
@@ -98,11 +135,10 @@ def growing_degree_days(base_temp, meantemp,planting_date, harvesting_date,freq=
     # if mean temp is lower than base temp, replace it with base temp, because crop can not grow below base temp.
     growingtime_meantemp.value=growingtime_meantemp.value.where(growingtime_meantemp.value>= base_temp)
     print "mean temperature in growing duration of crop is: "
+    growingtime_meantemp['value']=growingtime_meantemp['value'].replace(['NaN'], base_temp)
     print growingtime_meantemp
     print growingtime_meantemp.dtypes
     print type(growingtime_meantemp)
-    growingtime_meantemp['value']=growingtime_meantemp['value'].replace(['NaN'], base_temp)
-    print growingtime_meantemp
     # to calculate each days contribution to GDD
     eachday_gdd_contribution=growingtime_meantemp-base_temp
     print "each day contribution to growing degree days is: "
@@ -116,65 +152,25 @@ def growing_degree_days(base_temp, meantemp,planting_date, harvesting_date,freq=
     print growing_degree_days.dtypes
     print "growing degree days is "
     print growing_degree_days.tail(1)
+    
+    
+    
 
-
-# first read kisanhub.csv weather file using reading_weather_dataframe function and print weather dataframe
-print "Weather data of kisanhub.csv file is: " 
-weather_dataframe= reading_weather_dataframe ()
-print weather_dataframe
-print weather_dataframe.dtypes
-print type(weather_dataframe)
-
-print "After calculating missing weather data using linear interpolation method, Corrected weather data is:  "
-# calculate missing weather data using finding_missing_data function and print corrected weather dataframe
-corrected_weather_dataframe= finding_missing_data (weather_dataframe, "01/01/2014", "31/05/2016")
-print corrected_weather_dataframe 
-print corrected_weather_dataframe.dtypes
-print type( corrected_weather_dataframe)
-
-# to get daily dataframe from hourly weather dataframe resampling done using resample_dataframe function
-resample_data= resample_dataframe( corrected_weather_dataframe)
-print resample_data
-print type( resample_data)
-
-print "Daily maximum temperature of given time series:  "
-# to get daily maximum temperature of given datetime series maximum_temp function used
-max_temp=  maximum_temp(resample_data)
-print max_temp
-print max_temp.dtypes
-print type( max_temp)
-
-print "Daily minimum temperature of given time series:  "
-# daily minimum temperature get using minimum_temp function
-min_temp= minimum_temp(resample_data)
-print min_temp
-print min_temp.dtypes
-print type(min_temp)
-
-print "Daily mean temperature of given time series:  "
-# to get daily mean temperature of given datetime series mean_temp function used
-meantemp=mean_temp(max_temp, min_temp)
-print meantemp
-print meantemp.index
-#print meantemp['20140102':'20140104']
-#print meantemp[:1]
-print meantemp.dtypes
-print type(meantemp)
-
-# user can input planting date and convert it in correct datetime format using user_planting_date function
+# to calculate growing degree days of crop needs mean temperature which find using mean_temp_from_tempdataframe function 
+meantemp= meantemp_from_tempdatafrme()
+# to input planting and harvesting date which is essential variable to calculate GDD using user_planting_date 
+#and user_harvesting_date functions
 planting_date= user_planting_date()
 print planting_date
 print type (planting_date)
-# user can input harvesting date and convert it in correct datetime format using user_harvesting_date function
 harvesting_date= user_harvesting_date()
 print harvesting_date
 print type( harvesting_date)
-
 # user can input base temperature using user_base_temp function
 base_temp= user_base_temp()
 print base_temp
 print type(base_temp)
-
+# finally to calculate growing degree days, growing_degree_days function used with its parameters.
 growing_degree_days= growing_degree_days(base_temp, meantemp,planting_date, harvesting_date,freq='D')
 
 
